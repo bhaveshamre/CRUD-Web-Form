f4cd3ef4260f63ee69d41fb82708de9f83836b61
Bhaavesh Amol Amre <112196036+bhaveshamre@users.noreply.github.com>
2024-12-21 09:19:16 +0530

    Add files via upload

---
+++ WebForm.aspx
@@ -0,0 +1,93 @@
+﻿   <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="WebApplication9.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Registration</title>
+</head>
+<body>
+  <div class="container">
+    <div class="bg-primary p-4 rounded mb-3 mt-3 text-center">
+      <h1>Registration Form</h1>
+    </div>
+
+    <form class="bg-light row g-3" id="form1" runat="server" method="post">
+      <div class="col-md-6">
+        <label for="inputEmail4" class="form-label">Name</label>
+        <asp:TextBox ID="TextName" CssClass="form-control" runat="server"></asp:TextBox>
+    
+      </div>
+
+      <div class="col-md-6">
+        <label for="inputPassword4" class="form-label">Email</label>
+        <asp:TextBox id="txtemail" CssClass="form-control" runat="server" TextMode="Email"></asp:TextBox>
+          
+      </div>
+
+      <div class="col-12">
+        <label for="inputAddress" class="form-label">Address</label>
+        <asp:TextBox ID="Textadd" CssClass="form-control" runat="server"></asp:TextBox>
+      </div>
+
+      <div class="col-4">
+        <label for="inputAddress" class="form-label">Username</label>
+        <asp:TextBox ID="Textusername" CssClass="form-control" runat="server" TextMode="Password"></asp:TextBox>
+      </div>
+
+      <div class="col-md-4">
+        <label for="inputZip" class="form-label">File</label>
+        <asp:FileUpload ID="FileUpload1" CssClass="form-control" runat="server" />
+      </div>
+
+      <div class="col-md-4">
+        <label for="inputZip" class="form-label">DOB</label>
+        <asp:TextBox ID="TextBox1" CssClass="form-control" runat="server" TextMode="DateTimeLocal"></asp:TextBox>
+      </div>
+
+      <div class="col-md-6">
+        <label for="inputCity" class="form-label">Color</label><br />
+        <asp:RadioButtonList 
+          ID="RadioButtonList1" 
+          runat="server" 
+          CssClass="form-check">
+          <asp:ListItem Value="white">&nbsp;White</asp:ListItem>
+          <asp:ListItem Value="black">&nbsp; Black</asp:ListItem>
+        </asp:RadioButtonList>
+
+      </div>
+
+      <div class="col-md-4">
+        <label for="inputState" class="form-label">Gender</label>
+        <asp:DropDownList ID="DropDownList1" CssClass="form-control" runat="server">
+          <asp:ListItem Value="male">Male</asp:ListItem>
+          <asp:ListItem Value="female">Female</asp:ListItem>
+        </asp:DropDownList>
+      </div>
+
+      <div class="col-12">
+        <label for="sportsSelection" class="form-label">Sports</label>
+        <asp:CheckBoxList 
+          ID="SportsCheckBoxList" 
+          runat="server" 
+          CssClass="form-check">
+          <asp:ListItem Value="1">&nbsp;Soccer</asp:ListItem>
+          <asp:ListItem Value="2">&nbsp;Basketball</asp:ListItem>
+          <asp:ListItem Value="3">&nbsp;Tennis</asp:ListItem>
+          <asp:ListItem Value="4">&nbsp;Cricket</asp:ListItem>
+          <asp:ListItem Value="5">&nbsp;Baseball</asp:ListItem>
+        </asp:CheckBoxList>
+      </div>
+
+      <div class="col-12">
+        <asp:Button ID="Button1" CssClass="btn btn-primary" runat="server" Text="Submit" OnClick="Button1_Click" />
+          <asp:Button ID="Button2" CssClass="btn btn-primary" runat="server" Text="Update" OnClick="Button2_Click" />
+          <asp:Button ID="Button3" CssClass="btn btn-primary" runat="server" Text="Delete" OnClick="Button3_Click" />
+      </div>
+    </form>
+  </div>
+</body>
+
+</html>
---
+++ WebForm.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplication9
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;" +
+    "Initial Catalog=new;" +  
+    "Integrated Security=True;Connect Timeout=30;Encrypt=False;" +
+    "TrustServerCertificate=False;");
+
+            con.Open();
+            string insertQuery = "INSERT INTO Data (Name, Email, Address, Username, [File], Dob, Color, Gender, Sports) " +
+                     "VALUES (@Name, @Email, @Address, @Username, @File, @Dob, @Color, @Gender, @Sports)";
+
+            SqlCommand cmd = new SqlCommand(insertQuery, con);
+            cmd.Parameters.AddWithValue("@Name", TextName.Text);
+            cmd.Parameters.AddWithValue("@Email", txtemail.Text);
+            cmd.Parameters.AddWithValue("@Address", Textadd.Text);
+            cmd.Parameters.AddWithValue("@Username", Textusername.Text);
+            cmd.Parameters.AddWithValue("@File", FileUpload1.FileBytes);
+            cmd.Parameters.AddWithValue("@Dob", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@Color", RadioButtonList1.Text);
+            cmd.Parameters.AddWithValue("@Gender", DropDownList1.SelectedItem.ToString());
+            string selectedSports = string.Join(",", SportsCheckBoxList.Items.Cast<ListItem>()
+                                           .Where(i => i.Selected)
+                                           .Select(i => i.Value));
+            cmd.Parameters.AddWithValue("@Sports", selectedSports);
+            int count = cmd.ExecuteNonQuery();
+            //if(count>0)
+            //{
+               // Response.Write("File uploaded successfully.");
+            //}
+            //else
+            //{
+                //Response.Write("Please upload a file.");
+            //}
+            if (count > 0)
+            {
+                // Display success alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('File uploaded successfully.');", true);
+            }
+            else
+            {
+                // Display error alert
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Please upload a file.');", true);
+            }
+
+            con.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;" +
+                                                              "Initial Catalog=new;" +
+                                                              "Integrated Security=True;Connect Timeout=30;Encrypt=False;" +
+                                                              "TrustServerCertificate=False;"))
+                {
+                    con.Open();
+
+                    
+
+                    string updateQuery = "UPDATE Data SET Name = @Name, Email = @Email, Address = @Address, [File] = @File, Dob = @Dob, " +
+                                         "Color = @Color, Gender = @Gender, Sports = @Sports WHERE Name = @Name";
+
+                    using (SqlCommand cmd = new SqlCommand(updateQuery, con))
+                    {   
+                        cmd.Parameters.AddWithValue("@Name", TextName.Text);
+                        cmd.Parameters.AddWithValue("@Email", txtemail.Text);
+                        cmd.Parameters.AddWithValue("@Address", Textadd.Text);
+                        cmd.Parameters.AddWithValue("@Username", Textusername.Text);
+                        cmd.Parameters.AddWithValue("@File", FileUpload1.FileBytes);
+                        DateTime dob;
+                        if (!DateTime.TryParse(TextBox1.Text, out dob))
+                        {
+                            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Invalid date format.');", true);
+                            return;
+                        }
+                        cmd.Parameters.AddWithValue("@Dob", dob);
+                        cmd.Parameters.AddWithValue("@Color", RadioButtonList1.Text);
+                        cmd.Parameters.AddWithValue("@Gender", DropDownList1.SelectedItem.ToString());
+                        string selectedSports = string.Join(",", SportsCheckBoxList.Items.Cast<ListItem>()
+                                                       .Where(i => i.Selected)
+                                                       .Select(i => i.Value));
+                        cmd.Parameters.AddWithValue("@Sports", selectedSports);
+
+                        int count = cmd.ExecuteNonQuery();
+
+                        if (count > 0)
+                        {
+                            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Record updated successfully.');", true);
+                        }
+                        else
+                        {
+                            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Error updating record.');", true);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", $"alert('Error: {ex.Message}');", true);
+            }
+        }
+
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;" +
+                                                              "Initial Catalog=new;" +
+                                                              "Integrated Security=True;Connect Timeout=30;Encrypt=False;" +
+                                                              "TrustServerCertificate=False;"))
+                {
+                    con.Open();
+
+                    // Ensure a unique identifier is provided for the record to delete.
+                    if (string.IsNullOrEmpty(Textusername.Text))
+                    {
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Username is required to delete.');", true);
+                        return;
+                    }
+
+                    string deleteQuery = "DELETE FROM Data WHERE Name = @Name";
+
+                    using (SqlCommand cmd = new SqlCommand(deleteQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Name", Textusername.Text);
+
+                        int count = cmd.ExecuteNonQuery();
+
+                        if (count > 0)
+                        {
+                            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Record deleted successfully.');", true);
+                        }
+                        else
+                        {
+                            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('No record found to delete.');", true);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", $"alert('Error: {ex.Message}');", true);
+            }
+        }
+
+    }
+}
